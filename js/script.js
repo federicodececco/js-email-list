f1f1a28a633358d8743ded0b13a57a93eb268ebc
federicodececco <federicodececco1@gmail.com>
2025-01-08 20:57:39 +0100

    add: button

--- js/script.js
+++ js/script.js
@@ -14,20 +14,28 @@ for (let i = 0; i < 3; i++) {
 }
  */
 ///mcuh more complex code but modifyng the dom just once
-let listP = []
-let listString = ''
-for (let i = 0; i < 3; i++) {
-  listP.push(
-    axios
-      .get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(response => {
-        return response.data.response
-      }),
-  )
-}
-Promise.all(listP).then(results => {
-  for (let i = 0; i < 3; i++) {
-    listString += `<i class="list-group-item">${results[i]}</i>`
+/**
+ * functions that use boolean api to generate 10 random emails
+ * and get them trough axios, then uses promises to wait to receive the emails
+ * and inserts them in the HTML
+ */
+const promiseFunction = () => {
+  let listP = []
+  let listString = ''
+  for (let i = 0; i < 10; i++) {
+    listP.push(
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+          return response.data.response
+        }),
+    )
   }
-  document.getElementById('list').innerHTML = listString
-})
+  Promise.all(listP).then(results => {
+    for (let i = 0; i < 10; i++) {
+      listString += `<i class="list-group-item">${results[i]}</i>`
+    }
+    document.getElementById('list').innerHTML = listString
+  })
+}
+document.getElementById('buttone').addEventListener('click',promiseFunction)
